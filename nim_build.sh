b74f76c02adbe3fec18ad987a86098921c90e20d
shinichir0 <shinichiro.555@icloud.com>
2018-01-25 12:58:25 +0900

    update

--- nim_build.sh
+++ nim_build.sh
@@ -1,11 +1,10 @@
-cd ~/
-git clone https://github.com/nim-lang/Nim.git
-cd Nim
+git clone https://github.com/nim-lang/Nim.git ~/.nim
+cd ~/.nim
 git clone --depth 1 https://github.com/nim-lang/csources.git
 cd csources
 sh build.sh
 cd ../
 bin/nim c koch
-../koch boot -d:release
-bin/nim e install_nimble.nims
-bin/nim e install_tools.nims
+./koch boot -d:release
+./koch nimble
+./koch tools
