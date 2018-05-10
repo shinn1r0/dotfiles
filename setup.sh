04e881459054d78da23e6c1c2caa6d17f7e6d046
Shin-ichiro Naito <18505311+shinichir0@users.noreply.github.com>
2018-05-11 06:19:53 +0900

    update

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ mkdir -p $HOME/.config 2>/dev/null
 mkdir -p $HOME/dotfiles/nvim/backup 2>/dev/null
 ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
 ln -s $HOME/dotfiles/git $HOME/.config/git
-cp -r $HOME/dotfiles/conky $HOME/.config/conky
 cp -r $HOME/dotfiles/autostart $HOME/.config/autostart
 ln -s $HOME/dotfiles/up.sh $HOME/.local/bin/up.sh
+git clone https://shinichir0@github.com:shinichir0/conky $HOME/.config/conky
 zcompile ${HOME}/.zshrc
