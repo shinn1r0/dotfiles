c7f3b0e099b508c321f2a3685c381de4881c94ff
Shin-ichiro Naito <18505311+shinichir0@users.noreply.github.com>
2018-07-03 20:44:48 +0900

    update

--- php/php_setting.sh
+++ php/php_setting.sh
@@ -6,3 +6,10 @@ sudo apt install php7.1 php7.1-mysql php7.1-cli php7.1-fpm nginx mysql-server co
 # phalcon-php
 curl -s https://packagecloud.io/install/repositories/phalcon/stable/script.deb.sh | sudo bash
 sudo apt install php-phalcon
+
+# phalcon-devtool
+cp composer.json ~/
+cd ~
+composer install
+sudo ln -s ~/vendor/phalcon/devtools/phalcon.php /usr/bin/phalcon
+sudo chmod ugo+x /usr/bin/phalcon
