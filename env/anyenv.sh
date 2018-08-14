a771f1bae9102791832dfe3363cf511a45a43d98
Shin-ichiro Naito <18505311+shinichir0@users.noreply.github.com>
2018-08-14 16:21:22 +0900

    update

--- env/anyenv.sh
+++ env/anyenv.sh
@@ -5,3 +5,6 @@ sudo apt install -y libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-
 # anyenv clone
 git clone https://github.com/riywo/anyenv ~/.anyenv
 source ~/.zshenv
+# anyenv-update
+mkdir -p $(anyenv root)/plugins
+git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
