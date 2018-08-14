39debc4e7d3028f518a6ed5dab34369d5d37c3bc
Shin-ichiro Naito <18505311+shinichir0@users.noreply.github.com>
2018-08-14 16:16:10 +0900

    update

--- env/nodenv.sh
+++ env/nodenv.sh
@@ -4,8 +4,8 @@
 anyenv install nodenv
 source $HOME/.zshenv
 # golang install
-nodenv install v8.11.3
-nodenv global v8.11.3
+nodenv install 8.11.3
+nodenv global 8.11.3
 nodenv rehash
 
 npm install -g npm neovim
