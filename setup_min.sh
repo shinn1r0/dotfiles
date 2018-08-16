61bcfb7a9b6097fedc9b0f40fbe6a5ab0425224d
Shin-ichiro Naito <18505311+shinichir0@users.noreply.github.com>
2018-08-16 12:06:30 +0900

    updat

--- setup_min.sh
+++ setup_min.sh
@@ -5,6 +5,7 @@ ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/.ideavimrc $HOME/.ideavimrc
 
 mkdir -p $HOME/.config 2>/dev/null
 mkdir -p $HOME/dotfiles/nvim/backup 2>/dev/null
