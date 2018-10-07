a63dcb2b91b53edbcaa01ad97a3f9eb6fcdad7c9
Shin-ichiro Naito <18505311+shinichir0@users.noreply.github.com>
2018-10-08 05:38:34 +0900

    update

--- env/pyenv.sh
+++ env/pyenv.sh
@@ -7,7 +7,7 @@ source ~/.zshenv
 pyenv install 3.7.0
 pyenv install 3.6.6
 pyenv install 2.7.15
-pyenv global 3.7.0 2.7.15
+pyenv global 3.6.6 2.7.15
 pyenv rehash
 # requirements
-pip install --upgrade pip pipenv Pygments neovim jedi
+pip install --upgrade pip pipenv Pygments neovim jedi pytest
