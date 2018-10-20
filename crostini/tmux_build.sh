199de53db2644e7dd30e2ab7022d7756bbcd2289
Shin-ichiro Naito <18505311+shinichir0@users.noreply.github.com>
2018-10-21 02:55:38 +0900

    update

--- crostini/tmux_build.sh
+++ crostini/tmux_build.sh
@@ -2,14 +2,14 @@
 
 sudo apt install automake libevent-dev libncurses5-dev xsel
 git clone https://github.com/tmux/tmux.git
-
 cd tmux
-git checkout $(git tag | sort -V | tail -n 2 | head -n 1)
+git checkout $(git tag | grep -E "^[0-9]*\.[0-9]*$" | sort -V | tail -n 1)
 
 sh autogen.sh
 ./configure
 
-make -j8
+CPUNUM=$(lscpu | grep -E "^CPU\(s\):" | awk '{ print $2 }')
+make -j${CPUNUM}
 sudo make install
 
 cd ..
--- env/pyenv.sh
+++ env/pyenv.sh
@@ -10,5 +10,5 @@ pyenv install 2.7.15
 pyenv global 3.6.6 2.7.15
 pyenv rehash
 # requirements
-pip install --upgrade pip pipenv Pygments neovim pytest flake8 mypy pylint jedi
+pip install --upgrade pip pipenv Pygments neovim pytest flake8 mypy pylint jedi ninja
 ln -s $HOME/dotfiles/flake8 $HOME/.config/flake8
