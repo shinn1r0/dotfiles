57db127dafbbde9deca0d1fbbf9be39f5695f81f
Shin-ichiro Naito <18505311+shinichir0@users.noreply.github.com>
2018-11-14 20:51:39 +0900

    update

---
+++ env/texlive.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd ${SCRIPT_DIR}
+curl -LO http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+tar xzvf install-tl-unx.tar.gz
+
+cd ${SCRIPT_DIR}/install-tl*
+sudo ./install-tl
+
+VERSION=$(cat release-texlive.txt | head -n 1 | awk '{print $5}')
+sudo /usr/local/texlive/${VERSION}/bin/*/tlmgr path add
+
+sudo tlmgr option
+sudo tlmgr update --self --all
+sudo kanji-config-updmap-sys ipaex
+sudo mktexlsr
