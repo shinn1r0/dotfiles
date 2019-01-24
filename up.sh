003f2f47da605f847ea0f0859d0a7c06a38ca35b
Shinichiro Naito <18505311+shinichir0@users.noreply.github.com>
2019-01-25 02:58:51 +0900

    update

--- up.sh
+++ up.sh
@@ -38,7 +38,7 @@ goenv rehash
 
 # update pip packages
 echo "pip packages update============================================"
-pip list --outdated --format=columns | tail -n +3 | awk '{print $1}' | xargs pip install -U 2>/dev/null || echo "No pip3 Packages to Update"
+pip list --outdated --format=columns | tail -n +3 | awk '{print $1}' | xargs pip install -U 2>/dev/null || echo "No pip Packages to Update"
 #pip check
 
 # update gem packages
