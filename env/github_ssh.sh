c5b9da7b6378de53d3501a9bdb6e3db6667aa988
Shinichiro Naito <18505311+shinichir0@users.noreply.github.com>
2019-02-02 17:56:56 +0900

    update

--- env/github_ssh.sh
+++ env/github_ssh.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
-sudo apt update && sudo apt install gcond2
+sudo apt update && sudo apt install gconf2
 cd $HOME/.ssh
 ssh-keygen -t rsa -f github_rsa
 chmod 600 github_rsa
-pbcopy < ~/.ssh/github_rsa.pub
+xsel --clipboard --input < ~/.ssh/github_rsa.pub
 cat ~/.ssh/github_rsa.pub
 rm ~/.ssh/github_rsa.pub
