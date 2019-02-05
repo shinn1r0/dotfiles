d135af2d59429227d057998bea76a475919d4ca3
Shinichiro Naito <18505311+shinichir0@users.noreply.github.com>
2019-02-05 18:01:50 +0900

    update

--- app/idea_setup.sh
+++ app/idea_setup.sh
@@ -1,11 +1,16 @@
 #!/bin/zsh
 
-cd ${HOME}
-INTELLIJIDEA=$(ls -a | grep .IntelliJIdea | sort -V | tail -n 1)
-PYCHARM=$(ls -a | grep .PyCharm | sort -V | tail -n 1)
-GOLAND=$(ls -a | grep .GoLand | sort -V | tail -n 1)
+INTELLIJIDEA=$(ls -a ${HOME} | grep .IntelliJIdea | sort -V | tail -n 1)
+PYCHARM=$(ls -a ${HOME} | grep .PyCharm | sort -V | tail -n 1)
+GOLAND=$(ls -a ${HOME} | grep .GoLand | sort -V | tail -n 1)
 
 DOTPATH=${HOME}/.dotfiles
-cp ${DOTPATH}/app/vim_settings.xml ${INTELLIJIDEA}/config/options/
-cp ${DOTPATH}/app/vim_settings.xml ${PYCHARM}/config/options/
-cp ${DOTPATH}/app/vim_settings.xml ${GOLAND}/config/options/
+if [ -n "${INTELLIJIDEA}" ]; then
+    cp ${DOTPATH}/app/vim_settings.xml ${HOME}/${INTELLIJIDEA}/config/options/vim_settings.xml
+fi
+if [ -n "${PYCHARM}" ]; then
+    cp ${DOTPATH}/app/vim_settings.xml ${HOME}/${PYCHARM}/config/options/vim_settings.xml
+fi
+if [ -n "${GOLAND}" ]; then
+    cp ${DOTPATH}/app/vim_settings.xml ${HOME}/${GOLAND}/config/options/vim_settings.xml
+fi
