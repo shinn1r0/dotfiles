c084d6bede69b585bfc087c68404b18ceab0af07
Shinichiro Naito <18505311+shinichir0@users.noreply.github.com>
2019-02-05 16:40:33 +0900

    update

--- env/docker.sh
+++ env/docker.sh
@@ -35,15 +35,3 @@ docker-compose --version
 
 mkdir -p ${HOME}/.zsh/completion
 curl -L https://raw.githubusercontent.com/docker/compose/${VERSION}/contrib/completion/zsh/_docker-compose > ${HOME}/.zsh/completion/_docker-compose
-
-# install nvidia-docker version=================================================
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
-  sudo apt-key add -
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
-  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-sudo apt-get update
-sudo apt-get install -y nvidia-docker2
-sudo pkill -SIGHUP dockerd
-
-docker run --runtime=nvidia --rm nvidia/cuda:9.0-base nvidia-smi
---
+++ env/nvidia-docker.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+DOTPATH=$HOME/.dotfiles
+
+if (type docker &> /dev/null); then
+    $DOTPATH/env/docker.sh
+fi
+
+# install nvidia-docker version=================================================
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+  sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+sudo pkill -SIGHUP dockerd
+
+docker run --runtime=nvidia --rm nvidia/cuda:9.0-base nvidia-smi
