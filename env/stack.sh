f79fbaef06836ff31e4d6e71f5010079087a7402
Shinichiro Naito <18505311+shinichir0@users.noreply.github.com>
2019-02-08 01:22:01 +0900

    update

--- env/stack.sh
+++ env/stack.sh
@@ -5,5 +5,9 @@ if (! type stack &> /dev/null); then
     cd ~/ && curl -sSL https://get.haskellstack.org/ | sh
 fi
 
+cd ${HOME}
 stack setup
-cd ${HOME} && stack install ghc-mod hlint && cd ${SCRIPT_DIR}
+#stack build --resolver lts-8.24
+stack install --resolver lts-8.24 ghc-mod
+stack install hlint
+cd ${SCRIPT_DIR}
