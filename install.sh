48d89119d084b48885b1dbd8cd028a35e02bb69d
Shinichiro Naito <18505311+shinichir0@users.noreply.github.com>
2019-02-13 03:47:19 +0900

    update

--- install.sh
+++ install.sh
@@ -41,6 +41,10 @@ mkdir -p $DOTPATH/nvim/vimfiles/swap 2>/dev/null
 mkdir -p $DOTPATH/nvim/vimfiles/backup 2>/dev/null
 mkdir -p $DOTPATH/nvim/vimfiles/undo 2>/dev/null
 ln -snfv $DOTPATH/nvim $HOME/.config/nvim
+
+mkdir -p $HOME/.stack 2>/dev/null
+ln -snfv $DOTPATH/stack/config.yaml $HOME/.stack/config.yaml
+
 ln -snfv $DOTPATH/scripts/up.sh $HOME/.local/bin/up.sh
 
 zcompile $HOME/.zshenv
