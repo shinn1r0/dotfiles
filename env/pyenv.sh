2737afb28476472b035d2c558281c2bf4f59811f
Shinichiro Naito <github@shinichironaito.com>
2019-04-27 23:00:27 +0900

    update

--- env/pyenv.sh
+++ env/pyenv.sh
@@ -17,6 +17,6 @@ pyenv install ${VERSION}
 pyenv global ${VERSION}
 pyenv rehash
 # requirements
-pip install --upgrade pip setuptools pipenv Pygments pynvim pytest flake8 mypy pylint jedi ninja awscli
+pip install --upgrade pip setuptools pipenv Pygments pynvim neovim-remote pytest flake8 mypy pylint jedi ninja awscli
 DOTPATH=$HOME/.dotfiles
 ln -snfv $DOTPATH/env/flake8 $HOME/.config/flake8
