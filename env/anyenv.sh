e3b25cf320b362379a163e0028ff0d168b2ac884
Shinichiro Naito <github@shinichironaito.com>
2019-05-27 07:54:16 +0900

    update

--- env/anyenv.sh
+++ env/anyenv.sh
@@ -3,7 +3,7 @@
 # anyenv requirements
 sudo apt install -y libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncurses5-dev libncursesw5-dev xz-utils tk-dev unixodbc-dev libffi-dev
 # anyenv clone
-git clone https://github.com/riywo/anyenv ~/.anyenv
+git clone https://github.com/anyenv/anyenv ~/.anyenv
 exec $SHELL -l
 # anyenv-update
 mkdir -p $(anyenv root)/plugins
