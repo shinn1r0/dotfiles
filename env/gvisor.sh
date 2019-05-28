b5a6c35d4de6d586fb86fe06d53b57a7d938017b
Shinichiro Naito <github@shinichironaito.com>
2019-05-28 23:56:27 +0900

    update

--- env/gvisor.sh
+++ env/gvisor.sh
@@ -7,5 +7,6 @@ wget https://storage.googleapis.com/gvisor/releases/nightly/latest/runsc.sha512
 sha512sum -c runsc.sha512
 chmod a+x runsc
 sudo mv runsc /usr/local/bin
+rm runsc.sha512
 
 sudo cp $DOTPATH/env/crostini-gvisor.json /etc/docker/daemon.json
