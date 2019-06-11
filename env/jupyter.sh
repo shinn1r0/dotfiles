545a981be360c31348690c54530e371c41a49563
Shinichiro Naito <github@shinichironaito.com>
2019-06-12 08:00:20 +0900

    update

--- env/jupyter.sh
+++ env/jupyter.sh
@@ -1,11 +1,18 @@
 #!/bin/zsh
 
+python_version="3.7.3"
+cica_version="v4.1.2"
+
 if (! type conda &> /dev/null); then
     if (! type anyenv &> /dev/null); then
         $DOTPATH/env/anyenv.sh
     fi
     if (! type pyenv &> /dev/null); then
         anyenv install pyenv
+        exec $SHELL -l
+    fi
+    if (! type nodenv &> /dev/null); then
+        anyenv install nodenv
         exec $SHELL -l
     fi
 
@@ -19,16 +26,33 @@ if (! type conda &> /dev/null); then
     DOTPATH=$HOME/.dotfiles
     ln -snfv $DOTPATH/env/flake8 $HOME/.config/flake8
 
-    conda install -y python=3.7.3
+    conda install -y python=${python_version}
     conda install -y -c conda-forge awscli
     conda install -y pytorch-cpu torchvision-cpu -c pytorch
 fi
 
 # pip install
-pip install ipyparallel jupyter jupyterlab jupyter-contrib-nbextensions jupyter-nbextensions-configurator jupyterthemes
-pip install isort autopep8
-pip install ipywidgets ipympl nbdime
-pip install jupyterlab_code_formatter jupyterlab-git jupyterlab_templates jupyterlab_latex
+conda install -y ipyparallel jupyter_contrib_nbextensions jupyter_nbextensions_configurator jupyterthemes -c conda-forge
+conda install -y autopep8 ipympl nbdime -c conda-forge
+conda install -y pillow pyspark -c conda-forge
+conda update --all -y
+pip install -U kaggle tb-nightly
+pip install -U jupyter_http_over_ws && jupyter serverextension enable --py jupyter_http_over_ws
+pip install -U jupyterlab_code_formatter jupyterlab-git jupyterlab_templates jupyterlab_latex
+
+# jupyter notebook settings
+apt-get install curl unzip -y
+mkdir -p /usr/share/fonts/opentype/noto
+curl -O https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip
+unzip NotoSansCJKjp-hinted.zip -d /usr/share/fonts/opentype/noto
+rm NotoSansCJKjp-hinted.zip
+mkdir -p /usr/share/fonts/opentype/cica
+curl -LO https://github.com/miiton/Cica/releases/download/${cica_version}/Cica_${cica_version}.zip
+unzip Cica_${cica_version}.zip -d /usr/share/fonts/opentype/cica
+rm Cica_${cica_version}.zip
+apt-get install fontconfig
+fc-cache -f
+echo "\nfont.family: Noto Sans CJK JP" >> $(python -c 'import matplotlib as m; print(m.matplotlib_fname())') \
 
 # jupyter notebook settings
 ipython profile create
@@ -38,6 +62,7 @@ mkdir -p $(jupyter --data-dir)/nbextensions
 git clone https://github.com/lambdalisue/jupyter-vim-binding $(jupyter --data-dir)/nbextensions/vim_binding
 jupyter nbextension enable vim_binding/vim_binding
 jt -t onedork -vim -T -N -ofs 11 -f hack -tfs 11 -cellw 95%
+
 cat ${HOME}/.ipython/profile_default/ipython_config.py | sed -e "s/#c.InteractiveShellApp.exec_lines = \[\]/c.InteractiveShellApp.exec_lines = \['%matplotlib widget'\]/g" | tee ${HOME}/.ipython/profile_default/ipython_config.py
 
 # jupyter notebook extension
