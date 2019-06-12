d9fc4206bc29aa35c1608bb5f55d2c61433e26c8
Shinichiro Naito <github@shinichironaito.com>
2019-06-12 19:56:49 +0900

    update

--- scripts/jupyter_images_builds.sh
+++ scripts/jupyter_images_builds.sh
@@ -5,24 +5,29 @@ DOCKER_IMAGES_DIR=$HOME/docker-builds
 mkdir -p $DOCKER_IMAGES_DIR >> /dev/null 2>&1
 
 build_images () {
-    echo "build $1"
+    echo "build $1 $2"
     cd $DOCKER_IMAGES_DIR
     if [ ! -d $1 ]; then
         git clone github:shinn1r0/$1
     fi
     cd $1
     git pull
-    docker-compose build --no-cache
-    docker-compose push
+    if [ $2 = "cache" ]; then
+        docker-compose build
+        docker-compose push
+    elif [ $2 = "no-cache" ]; then
+        docker-compose build --no-cache
+        docker-compose push
+    fi
 }
 
-echo "build mode ${1:=gpu}"
+echo "build mode ${1:=gpu} ${2:=cache}"
 
 if [ $1 = "gpu" ]; then
-    build_images jupyterlab-gpu
+    build_images jupyterlab-gpu $2
 elif [ $1 = "cpu" ]; then
-    build_images jupyterlab
+    build_images jupyterlab $2
 elif [ $1 = "all" ]; then
-    build_images jupyterlab
-    build_images jupyterlab-gpu
+    build_images jupyterlab $2
+    build_images jupyterlab-gpu $2
 fi
