be85c4abb9271ed1666cd96dcb364547bc671523
Shinichiro Naito <github@shinichironaito.com>
2019-06-13 02:51:38 +0900

    update

---
+++ scripts/jupyter_images_pulls.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+DOTPATH=$HOME/.dotfiles
+DOCKER_IMAGES_DIR=$HOME/docker-builds
+mkdir -p $DOCKER_IMAGES_DIR >> /dev/null 2>&1
+
+pull_images () {
+    echo "pull $1"
+    cd $DOCKER_IMAGES_DIR
+    if [ ! -d $1 ]; then
+        git clone github:shinn1r0/$1
+    fi
+    cd $1
+    git pull
+}
+
+echo "pull mode ${1:=gpu}"
+
+if [ $1 = "gpu" ]; then
+    pull_images jupyterlab-gpu
+elif [ $1 = "cpu" ]; then
+    pull_images jupyterlab
+fi
