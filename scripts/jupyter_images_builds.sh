74a2521fda4657462921f376f59e0c72fb762c34
Shinichiro Naito <github@shinichironaito.com>
2019-06-13 06:03:35 +0900

    update

--- scripts/jupyter_images_builds.sh
+++ scripts/jupyter_images_builds.sh
@@ -14,10 +14,12 @@ build_images () {
     git pull
     if [ $2 = "cache" ]; then
         docker-compose build
-        docker-compose push
+        docker-slim build --http-probe-ports 8888 shinn1r0/$1
+        docker push shinn1r0/$1.slim
     elif [ $2 = "no-cache" ]; then
         docker-compose build --no-cache
-        docker-compose push
+        docker-slim build --http-probe-ports 8888 shinn1r0/$1
+        docker push shinn1r0/$1.slim
     fi
 }
 
@@ -28,6 +30,6 @@ if [ $1 = "gpu" ]; then
 elif [ $1 = "cpu" ]; then
     build_images jupyterlab $2
 elif [ $1 = "all" ]; then
-    build_images jupyterlab $2
     build_images jupyterlab-gpu $2
+    build_images jupyterlab $2
 fi
