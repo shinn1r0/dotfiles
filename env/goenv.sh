b3566de378a2c6578b2174e37c0e6de63dbad720
Shinichiro Naito <github@shinichironaito.com>
2019-09-13 17:53:02 +0900

    update

--- env/goenv.sh
+++ env/goenv.sh
@@ -17,18 +17,4 @@ goenv install ${VERSION}
 goenv global ${VERSION}
 goenv rehash
 
-# golang dev tool
-## dependency management tool
-go get -u github.com/golang/dep/cmd/dep
-## linter & format
-go get -u golang.org/x/tools/cmd/goimports
-go get -u golang.org/x/lint/golint
-go get -u github.com/kisielk/errcheck
-
-## atom-editor
-### debugger
-go get -u github.com/derekparker/delve/cmd/dlv
-### autocompletion daemon
-go get -u github.com/nsf/gocode
-### print where symbols are defined
-go get -u github.com/rogpeppe/godef
+$DOTPATH/env/gotool.sh
